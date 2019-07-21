1a722033eec75f69fbf284dbc12eeb3101acb85b
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-07-20 19:09:43 -0700

    changes to heroku

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-test'
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-test',
+  //   API_ENDPOINT: `https://thawing-mesa-25904.herokuapp.com/`,
+  // API_KEY: process.env.REACT_APP_API_KEY,
   }
+  
